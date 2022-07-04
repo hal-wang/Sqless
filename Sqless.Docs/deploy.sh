3fc92d45b9b968706abf14d7784327aef9088ebd
hal-wang <hi@hal.wang>
2022-07-04 17:26:13 +0800

    chore(docs): update scripts

--- Sqless.Docs/deploy.sh
+++ Sqless.Docs/deploy.sh
@@ -1,6 +1,6 @@
 cd docs/.vuepress/dist/
-git init
+git init -b gh-pages
 git add -A
 git commit -m deploy
-git remote add origin https://github.com/hal-wang/sqless-docs.git
-git push origin master -f
+git remote add origin git@github.com:hal-wang/Sqless.git
+git push origin gh-pages -f
